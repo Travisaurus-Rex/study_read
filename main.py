3e6ec218695931aab0c4c52d71063a93e412298d
Travis Adams <brokenCD@live.com>
2025-11-10 10:54:15 +0700

    create lifespan context

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.routes import health
 from app.core.config import settings
+from app.db.session import engine
+from app.db.base import Base
 
 app = FastAPI(title=settings.app_name)
 app.include_router(health.router)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
 
 @app.get("/")
 def read_root():
